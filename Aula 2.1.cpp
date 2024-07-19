82c4b1f388c4e605e81b20e858cd3616f157de66
wemersonsillva <77592267+wemerson1997@users.noreply.github.com>
2024-07-18 21:15:32 -0300

    Aulas
    Aul 2.e 2.2

---
+++ Aula 2.1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	printf ("Olá, eu sou Wemerson Silva \n");
+	
+	printf ("Valor inteiro %d. \n", 10);
+	printf ("Valor real %f. \n", 3.141259875);
+	printf ("Valor real com apenas duas casas:  %.2f. \n", 4.456123);
+	printf ("Dado de texto:  %c. \n", 'a');
+	printf ("Valor inteiro %s. \n", "testando");
+}
---
+++ aula 2.2 entrada de dados - scanf.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+	int idade = 0;
+	
+	printf ("Digite sua idade: %d. \n", idade);
+	
+	printf("Digite uam idade:");
+	scanf("%d", &idade);
+	
+	printf("Idade informada: %d. \n", idade);
+}
